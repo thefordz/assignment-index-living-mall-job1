049f8fae6940856cea3d30523ff36ebfb6a48cca
Krekrith Sanittiveat <thefordz@Krekriths-MacBook-Air.local>
2026-02-16 18:11:18 +0700

    fix(header) : edit header
    - add sticky
    - background color
    - z-index

--- features/layouts/components/header.tsx
+++ features/layouts/components/header.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export function Header() {
   return (
-    <header className="h-16 w-full">
+    <header className="h-16 w-full sticky top-0 bg-background z-50">
       <div className="px-3 h-full w-full flex items-center justify-between border-b-3">
-        Pokemon List
+        <Link href={"/"} className="text-2xl font-bold">
+          Pokemon List
+        </Link>
       </div>
     </header>
   );
